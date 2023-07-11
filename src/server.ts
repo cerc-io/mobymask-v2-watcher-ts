cd3ea124d4b17ee44b07422fd22abb2f4fa47bc1
Nabarun <nabarun@deepstacksoft.com>
2023-07-11 15:17:13 +0530

    Instantiate nitro client with watcher peer

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,27 @@ import { ethers } from 'ethers';
 
 import { ServerCmd } from '@cerc-io/cli';
 
+// TODO: Publish package
+import {
+  P2PMessageService,
+  Client,
+  EthChainService,
+  PermissivePolicy,
+  DurableStore
+
+} from '@cerc-io/nitro-client';
+// TODO: Publish package
+import { hex2Bytes } from '@cerc-io/nitro-util';
+
 import { createResolvers } from './resolvers';
 import { Indexer } from './indexer';
 import { Database } from './database';
 import { createMessageToL2Handler, parseLibp2pMessage } from './libp2p-utils';
+import {
+  nitroAdjudicatorAddress,
+  virtualPaymentAppAddress,
+  consensusAppAddress
+} from './nitro-addresses.json';
 
 const log = debug('vulcanize:server');
 
@@ -33,7 +50,49 @@ export const main = async (): Promise<any> => {
   }
 
   const typeDefs = fs.readFileSync(path.join(__dirname, 'schema.gql')).toString();
-  return serverCmd.exec(createResolvers, typeDefs, p2pMessageHandler);
+  await serverCmd.exec(createResolvers, typeDefs, p2pMessageHandler);
+  await setupNitro(serverCmd);
+};
+
+const setupNitro = async (serverCmd: ServerCmd): Promise<Client | undefined> => {
+  const {
+    server: {
+      p2p: {
+        enablePeer,
+        nitro
+      }
+    },
+    upstream: {
+      ethServer: {
+        rpcProviderEndpoint
+      }
+    }
+  } = serverCmd.config;
+
+  if (!enablePeer) {
+    return;
+  }
+
+  // TODO: Use serverCmd.peer private key for nitro-client?
+  const store = DurableStore.newDurableStore(hex2Bytes(nitro.privateKey), path.resolve(nitro.store));
+  assert(serverCmd.peer);
+  const msgService = await P2PMessageService.newMessageService(store.getAddress(), serverCmd.peer);
+
+  const chainService = await EthChainService.newEthChainService(
+    rpcProviderEndpoint,
+    nitro.chainPrivateKey,
+    nitroAdjudicatorAddress,
+    consensusAppAddress,
+    virtualPaymentAppAddress
+  );
+
+  return Client.new(
+    msgService,
+    chainService,
+    store,
+    undefined,
+    new PermissivePolicy()
+  );
 };
 
 main().then(() => {
